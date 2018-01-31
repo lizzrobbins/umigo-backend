f02f4a7f0e55795a21757ca1619c93f01a586d71
kylegibson15 <kylegibson15@gmail.com>
2018-01-31 09:16:42 -0700

    added route to verify user login information

--- routes/people.js
+++ routes/people.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const query = require('../db/query')
 
 // read
+router.post('/login', (req, res, next) => {
+  query.verifyLogin(req.body.data).then(data => {
+    res.json(data)
+  })
+});
 router.get('/', (req, res) => {
-  query.getPeople()
-    .then((people) => {
-      res.json(people)
-    })
+  query.getPeople().then((people) => {
+    res.json(people)
+  })
 })
 
-router.get('/people/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   let id = req.params.id
-  query.getPeopleById(id)
-    .then((people) => {
-      res.json(people)
-    })
+  query.getPeopleById(id).then((people) => {
+    res.json(people)
+  })
 })
 
 module.exports = router;
